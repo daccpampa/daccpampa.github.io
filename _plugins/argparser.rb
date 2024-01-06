5a755e289bb21f3d080e915ecd1727222b7bde51
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2024-01-06 09:00:29 -0300

    Refactor ArgParser

--- _plugins/argparser.rb
+++ _plugins/argparser.rb
@@ -5,15 +5,15 @@ module ArgParser
     # This is a very simple argument parser. It only supports
     # arguments that are either bare words or quoted strings.
     # Commas are treated as argument separators, unless they
-    # are inside quotes. There is no escaping.
+    # are inside quotes. There is no string escaping.
 
-    arguments = StringScanner.new(markup.to_s.strip)
-    args = []
-    while !arguments.eos?
-      args << arguments.scan(/(?:[^,"']|"[^"]*"|'[^']*')+/)
-      arguments.skip(/\s*,\s*/)
+    args = StringScanner.new(markup.to_s.strip)
+    argv = []
+    while !args.eos?
+      argv << args.scan(/(?:[^,"']|"[^"]*"|'[^']*')+/)
+      args.skip(/\s*,\s*/)
     end
-    args.map { |arg| arg.gsub(/\A['"]|['"]\Z/, '') }
+    argv.map { |arg| arg.gsub(/\A['"]|['"]\Z/, '') }
   end
 
   module_function :parse_tag_arguments
